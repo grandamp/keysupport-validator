f6192605e96cab29f11f5e2e78ee4ef5afe341a0
Todd Johnson <grandamp@gmail.com>
2026-07-31 16:21:48 -0400

    Bump version to 1.0.2-beta.2 (Version Code 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "net.keysupport.validator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.2-beta.1"
+        versionCode = 5
+        versionName = "1.0.2-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
