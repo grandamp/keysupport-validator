40272930cd0a6629eb478bde9c473a28f3f61c04
Todd Johnson <grandamp@gmail.com>
2026-07-31 16:37:22 -0400

    Fix launcher icon path and bump version to 1.0.2-beta.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "net.keysupport.validator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.2-beta.2"
+        versionCode = 6
+        versionName = "1.0.2-beta.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
